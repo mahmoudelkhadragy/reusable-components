dd339a263825fa1c6e64eb6e148c4e5765ec76fa
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-11-06 21:27:32 +0200

    add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from './components/forms/forms.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'forms', component: FormsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { FaInputComponent } from './components/forms/fa-input/fa-input.component';
+import { InputRefDirective } from './common/input-ref.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FormsComponent, FaInputComponent, InputRefDirective],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
