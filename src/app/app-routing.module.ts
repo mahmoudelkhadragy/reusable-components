68442c26e6c3315334471946dee94575342a694a
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-11-09 05:28:56 +0200

    add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsComponent } from './components/forms/forms.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [{ path: 'forms', component: FormsComponent }];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'forms', component: FormsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { FaInputComponent } from './components/forms/fa-input/fa-input.component';
 import { InputRefDirective } from './common/input-ref.directive';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent, FormsComponent, FaInputComponent, InputRefDirective],
+  declarations: [AppComponent, FormsComponent, FaInputComponent, InputRefDirective, HomePageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
